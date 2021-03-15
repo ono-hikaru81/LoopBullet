6a5d193c085df0e9fb92a932ca264f4180f44c12
Miyoshi-Takuto <miyoshi.taku10@gmail.com>
2021-03-16 02:19:12 +0900

    設定画面のUIを仮置き

---
+++ Assets/Scripts/GameOption.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOption : MonoBehaviour {
+    public GameObject cursor;
+
+    public GameObject[] options;
+    public int currentSelect;
+
+    // Start is called before the first frame update
+    void Start () {
+        currentSelect = 0;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if ( Input.GetKeyDown( KeyCode.UpArrow ) ) {
+        currentSelect--;
+            if ( currentSelect < 0 ) {
+                currentSelect = 0;
+            }
+        }
+        else if ( Input.GetKeyDown( KeyCode.DownArrow ) ) {
+            currentSelect++;
+            if ( currentSelect >= options.Length ) {
+                currentSelect = options.Length - 1;
+            }
+        }
+
+        Vector3 temp = options[currentSelect].transform.localPosition;
+        temp.x = 0;
+        cursor.transform.localPosition = temp;
+    }
+}
---
+++ Assets/Scripts/Title/ChangeScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScreen : MonoBehaviour
+{
+    public GameObject[] screens;
+    int currentScreen;
+
+    private void Start () {
+        foreach(GameObject temp in screens ) {
+            temp.SetActive( false );
+        }
+
+        screens[0].SetActive( true );
+        currentScreen = 0;
+    }
+
+    public void Exec ( int nextScreen ) {
+        if ( nextScreen >= screens.Length || nextScreen < 0 ) nextScreen = screens.Length - 1;
+        screens[nextScreen].SetActive( true );
+        screens[currentScreen].SetActive( false );
+    }
+}
