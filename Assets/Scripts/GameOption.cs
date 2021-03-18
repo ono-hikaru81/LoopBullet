56cf2f567008d679a966327ef5db374e9cc0e79e
Miyoshi-Takuto <miyoshi.taku10@gmail.com>
2021-03-18 16:31:23 +0900

    オプションのスライダーを実装

--- Assets/Scripts/GameOption.cs
+++ Assets/Scripts/GameOption.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOption : MonoBehaviour {
     public GameObject cursor;
+    public Text description;
 
     public GameObject[] options;
     public int currentSelect;
@@ -11,25 +13,40 @@ public class GameOption : MonoBehaviour {
     // Start is called before the first frame update
     void Start () {
         currentSelect = 0;
+        options[0].GetComponent<OptionData>().IsControl( true );
     }
 
     // Update is called once per frame
     void Update () {
         if ( Input.GetKeyDown( KeyCode.UpArrow ) ) {
-        currentSelect--;
+            currentSelect--;
             if ( currentSelect < 0 ) {
                 currentSelect = 0;
             }
+
+            DescriptionUpdate();
         }
         else if ( Input.GetKeyDown( KeyCode.DownArrow ) ) {
             currentSelect++;
             if ( currentSelect >= options.Length ) {
                 currentSelect = options.Length - 1;
             }
+
+            DescriptionUpdate();
         }
 
-        Vector3 temp = options[currentSelect].transform.localPosition;
-        temp.x = 0;
-        cursor.transform.localPosition = temp;
+        // カーソルの位置調整
+        Vector3 vec = options[currentSelect].transform.localPosition;
+        vec.x = 0;
+        cursor.transform.localPosition = vec;
+    }
+
+    void DescriptionUpdate () {
+        description.text = options[currentSelect].GetComponent<OptionData>().GetDescription();
+        foreach(GameObject temp in options ) {
+            temp.GetComponent<OptionData>().IsControl( false );
+        }
+
+        options[currentSelect].GetComponent<OptionData>().IsControl( true );
     }
 }
---
+++ Assets/Scripts/OptionData.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionData : MonoBehaviour
+{
+    public string description;
+
+    public GameObject slider;
+    public GameObject ping;
+    public int maxValue;
+    public int value;
+    int valuePer;
+    RectTransform rect;
+    bool selected;
+
+    // Start is called before the first frame update
+    void Start () {
+        rect = slider.GetComponent<RectTransform>();
+        valuePer = ( int ) rect.sizeDelta.x / maxValue;
+        selected = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Control();
+
+        // ピンの位置調整
+        Vector3 vec = slider.transform.localPosition;
+        vec.x += valuePer * value;
+        vec.x -= rect.sizeDelta.x / 2;
+        ping.transform.localPosition = vec;
+    }
+
+    void Control () {
+        if ( selected == false ) return;
+
+        // ピンの操作
+        if( Input.GetKeyDown( KeyCode.LeftArrow ) ) {
+            value--;
+            if( value < 0 ) {
+                value = 0;
+            }
+        }
+        else if ( Input.GetKeyDown( KeyCode.RightArrow ) ) {
+            value++;
+            if( value > maxValue ) {
+                value = maxValue;
+            }
+        }
+    }
+
+    public string GetDescription () {
+        return description;
+    }
+
+    public void IsControl ( bool value ) {
+        selected = value;
+    }
+}
