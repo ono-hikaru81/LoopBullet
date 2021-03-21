338e45252259fc183eb8e974c618974a6474c699
ono-hikaru81 <Onohikaru0801@gmail.com>
2021-03-22 03:11:58 +0900

    重力が効くようになったよ！
    Playerに重力が効くように修正。

--- Assets/Scripts/FauxGravityAttractor.cs
+++ Assets/Scripts/FauxGravityAttractor.cs
@@ -18,12 +18,7 @@ public class FauxGravityAttractor : MonoBehaviour
         Vector3 gravityUp = (body.position - transform.position).normalized;
         Vector3 bodyUp = body.up;
 
-        // 下の　"rb.AddForce(gravityUp * gravity);" を　"body.ridbody.AddForce(gravityUp * gravity);" に直せれば惑星に沿って移動できるかも..?
-        // 現状古いバージョンの資料を見て作ったから "body.ridbody.AddForce(gravityUp * gravity);" で作るとエラーを吐きやがる。
-        // くそが
-        // 弾に使うなら現状が最適化かも..?   
-        rb.AddForce(gravityUp * gravity);
-        // body.ridbody.AddForce(gravityUp * gravity);
+        body.GetComponent<Rigidbody>().AddForce(gravityUp * gravity);
 
         Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, gravityUp) * body.rotation;
         body.rotation = Quaternion.Slerp(body.rotation, targetRotation, 50 * Time.deltaTime);
