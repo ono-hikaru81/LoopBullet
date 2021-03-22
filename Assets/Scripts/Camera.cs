fe2318788d8d66bbdf45ee03877f9ce5158479b1
Miyoshi-Takuto <miyoshi.taku10@gmail.com>
2021-03-22 15:50:41 +0900

    外国人の動画様
    キャラクターの移動とカメラ

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject planet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        // Position
+        transform.position = Vector3.Lerp( transform.position, player.transform.position, 0.05f );
+
+        // Rotation
+        transform.rotation = player.transform.rotation;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject planet;
+    public float speed;
+
+    float gravity;
+    bool onGround;
+
+    float distanceToGround;
+    Vector3 groundNormal;
+
+    Vector3 latestPos;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gravity = 100;
+        onGround = false;
+
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movement
+        float x = Input.GetAxis( "Horizontal" ) * Time.deltaTime * speed;
+        float z = Input.GetAxis( "Vertical" ) * Time.deltaTime * speed;
+
+        transform.Translate( x, 0, z );
+
+        // Gravity
+        RaycastHit hit = new RaycastHit();
+        if( Physics.Raycast(transform.position, -transform.up, out hit, 10 ) ) {
+            distanceToGround = hit.distance;
+            groundNormal = hit.normal;
+
+            onGround = ( distanceToGround <= 0.2f ) ? true : false;
+        }
+
+        Vector3 gravDirection = ( transform.position - planet.transform.position ).normalized;
+
+        if( onGround == false ) {
+            rb.AddForce( gravDirection * -gravity );
+        }
+
+        // Rotation
+        if( Input.GetButton( "CamRight" ) ) {
+            transform.Rotate( 0, 150 * Time.deltaTime, 0 );
+        }
+        if( Input.GetButton( "CamLeft" ) ) {
+            transform.Rotate( 0, -150 * Time.deltaTime, 0 );
+        }
+
+        // 惑星に向けて平行をとる
+        Quaternion toRotation = Quaternion.FromToRotation( transform.up, groundNormal ) * transform.rotation;
+        transform.rotation = toRotation;
+    }
+}
