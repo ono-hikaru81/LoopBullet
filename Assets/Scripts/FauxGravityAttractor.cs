83e18a870c534b1e7f259c91bdafdab99f631f27
ono-hikaru81 <Onohikaru0801@gmail.com>
2021-03-25 21:39:15 +0900

    調整
    ・プレイヤーと惑星をプレハブ化
    ・ちょっとした調整

--- Assets/Scripts/FauxGravityAttractor.cs
+++ Assets/Scripts/FauxGravityAttractor.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FauxGravityAttractor : MonoBehaviour
 {
-    public float gravity = -10;
+    public float gravity = 10;
     private Rigidbody rb;
 
     void Start()
@@ -18,7 +18,7 @@ public class FauxGravityAttractor : MonoBehaviour
         Vector3 gravityUp = (body.position - transform.position).normalized;
         Vector3 bodyUp = body.up;
 
-        body.GetComponent<Rigidbody>().AddForce(gravityUp * gravity);
+        body.GetComponent<Rigidbody>().AddForce(gravityUp * -gravity);
 
         Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, gravityUp) * body.rotation;
         body.rotation = Quaternion.Slerp(body.rotation, targetRotation, 50 * Time.deltaTime);
