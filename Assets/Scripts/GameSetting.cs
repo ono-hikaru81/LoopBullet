2edd7c0bc3270b25be3065d1ae402bd6780408e4
Miyoshi-Takuto <miyoshi.taku10@gmail.com>
2021-04-14 12:18:58 +0900

    接続画面の実装
    接続画面でXを押したプレイヤーのみゲームに参加する

--- Assets/Scripts/GameSetting.cs
+++ Assets/Scripts/GameSetting.cs
@@ -27,9 +27,45 @@ public class GameSetting : MonoBehaviour {
         set { scene = value; }
     }
 
+    public struct PlayerData {
+        bool join;
+        public bool Join {
+            get { return join; }
+            set { join = value; }
+        }
+        string horizontal;
+        public string Horizontal {
+            get { return horizontal; }
+        }
+        string vertical;
+        public string Vertical {
+            get { return vertical; }
+        }
+        string shot;
+        public string Shot {
+            get { return shot; }
+        }
+        string item;
+        public string Item {
+            get { return item; }
+        }
+
+        public PlayerData( bool join_, string horizontal_, string vertical_, string shot_, string item_ ) {
+            join = join_;
+            horizontal = horizontal_;
+            vertical = vertical_;
+            shot = shot_;
+            item = item_;
+        }
+    }
+    static PlayerData[] buttonBinds = new PlayerData[4];
+    public PlayerData[] ButtonBinds {
+        get { return buttonBinds; }
+        set { buttonBinds = value; }
+    }
+
     // Start is called before the first frame update
     void Start () {
-
     }
 
     // Update is called once per frame
